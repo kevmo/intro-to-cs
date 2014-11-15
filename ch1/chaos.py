461788b01c8dfe7ab07d07d64bc1423bf38bc2be
kevmo <k.james.moore@gmail.com>
2014-11-15 09:57:10 -0600

    Chapter 1

---
+++ ch1/chaos.py
@@ -0,0 +1,10 @@
+def main():
+  x = input("Enter a number between 0 and 1: ")
+  y = input("Enter another number between 0 and 1:")
+  for i in range(11):
+    x = x * (1 - x) * 3.7
+    y = y * (1-y) * 3.7
+    print "|  ", x, "  |  ", y, "  |  "
+
+if __name__ == '__main__':
+  main()
